5ad307eca64941d0d22b7934b757f1867cc027df
IIODERII <oderrisi@gmail.com>
2023-11-23 12:05:58 +0100

    added element to scroll up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <MainPage v-if="store.page === 'home'" />
   <SearchPage v-else-if="store.page === 'search'" />
+  <div id="chevron" @click="scrollUp">
+    <i class="fa-solid fa-chevron-up"></i>
+  </div>
 </template>
 
 <script>
@@ -19,6 +22,11 @@ export default {
     MainPage,
     SearchPage,
   },
+  methods: {
+    scrollUp() {
+      window.scrollTo(0, 0);
+    },
+  },
   // methods: {
   //   callMovies() {
   //     const url = this.store.apiUrl + this.store.endPoint;
@@ -33,4 +41,24 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "./assets/styles/partials/variables" as *;
+#chevron {
+  position: fixed;
+  bottom: 25px;
+  right: 25px;
+  width: 3em;
+  height: 3em;
+  background-color: $textColor;
+  color: $backgroundColor;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: $primaryColor;
+    color: $textColor;
+  }
+}
+</style>
